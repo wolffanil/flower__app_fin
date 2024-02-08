d712bba61648c4a347065bfcd6c4fa5774c2acbc
wolffanil2 <wolffanil@mail.ru>
2024-02-08 20:45:42 +0400

    first commit 2

--- components/screen/pages/Catalog/SortButton/SortButton.jsx
+++ components/screen/pages/Catalog/SortButton/SortButton.jsx
@@ -1,5 +1,5 @@
-import styled from "./sortButton.module.css";
-import SortForm from "../sortForm/SortForm";
+import styled from "./SortButton.module.css";
+import SortForm from "../SortForm/SortForm";
 
 export const SortButton = () => {
   return (
